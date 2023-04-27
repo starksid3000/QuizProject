ff73d9d37ee84c2e56a35eea8626653df64b631f
sonis33 <sonis33>
2023-04-27 17:08:28 +0100

    new commit

---
+++ Quize/js/questions.js
@@ -0,0 +1,168 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "Which country won the 2018 FIFA World Cup?",
+    answer: "France",
+    options: [
+      "Brazil",
+      "France",
+      "Germany",
+      "Spain"
+    ]
+  },
+    {
+    numb: 2,
+    question: "What is the highest score you can achieve in a single throw in a game of ten-pin bowling?",
+    answer: "10",
+    options: [
+      "10",
+      "50",
+      "100",
+      "300"
+    ]
+  },
+    {
+    numb: 3,
+    question: "In which sport would you use a shuttlecock?",
+    answer: "Badminton",
+    options: [
+      "Tennis",
+      "Badminton",
+      "Squash",
+      "Table Tennis"
+    ]
+  },
+    {
+    numb: 4,
+    question: "Which sport is played on the largest pitch?",
+    answer: "Cricket",
+    options: [
+      "Soccer",
+      "American Football",
+      "Rugby",
+      "Cricket"
+    ]
+  },
+    {
+    numb: 5,
+    question: "Who holds the record for the most Grand Slam titles in men's tennis?",
+    answer: "Roger Federer",
+    options: [
+      "Roger Nadal",
+      "Roger Federer",
+      "Novak Djokovic",
+      "Pete Sampras"
+    ]
+  },
+    {
+    numb: 6,
+    question: "Which country has won the most Olympic gold medals in history?",
+    answer: "United States",
+    options: [
+      "United States",
+      "Russia",
+      "China",
+      "Germany"
+    ]
+  },
+  {
+    numb: 7,
+    question: "What is the name of the trophy awarded to the winner of the UEFA Champions League?",
+    answer: "Champions League Trophy",
+    options: [
+      "Stanley Cup",
+      "Vince Lombardi Trophy",
+      "Lombardi Trophy",
+      "Champions League Trophy"
+    ]
+  },
+  {
+    numb: 8,
+    question: "What is the distance of a marathon race?",
+    answer: "26.2 miles",
+    options: [
+      "13.1 miles",
+      "26.2 miles",
+      "39.3 miles",
+      "54.4 miles"
+    ]
+  },
+  {
+    numb: 9,
+    question: "Who is the all-time leading scorer in NBA history?",
+    answer: "Kareem Abdul-Jabbar",
+    options: [
+      "Kobe Bryant",
+      "LeBron James",
+      "Michael Jordan",
+      "Kareem Abdul-Jabbar"
+    ]
+  },
+  {
+    numb: 10,
+    question: "What is the name of the annual golf tournament held at Augusta National Golf Club?",
+    answer: "The Masters",
+    options: [
+      "The Players Championship",
+      "The Masters",
+      "The U.S. Open",
+      "The Open Championship"
+    ]
+  },
+  {
+    numb: 11,
+    question: "Which country won the 2019 Cricket World Cup?",
+    answer: "England",
+    options: [
+      "India",
+      "England",
+      "China",
+      "Germany"
+    ]
+  },
+  {
+    numb: 12,
+    question: "Which boxer holds the record for the most consecutive wins in professional boxing",
+    answer: "Rocky Marciano",
+    options: [
+      "Floyd Mayweather Jr.",
+      "Rocky Marciano",
+      "Muhammad Ali",
+      "Mike Tyson"
+    ]
+  },
+  {
+    numb: 13,
+    question: "What is the name of the stadium that hosts the Wimbledon tennis tournament?",
+    answer: "All England Lawn Tennis and Croquet Club",
+    options: [
+      "Wembley Stadium",
+      "Old Trafford",
+      "All England Lawn Tennis and Croquet Club",
+      "Emirates Stadium"
+    ]
+  },
+  {
+    numb: 14,
+    question: "In which year did the Summer Olympics first take place?",
+    answer: "1896",
+    options: [
+      "1896",
+      "1900",
+      "1904",
+      "1908"
+    ]
+  },
+  {
+    numb: 15,
+    question: "What is the name of the annual cycling race held in France?",
+    answer: "Tour de France",
+    options: [
+      "Tour de France",
+      "Giro d'Italia",
+      "Vuelta a España",
+      "Tour of California"
+    ]
+  },
+];
